e95f4e84da4c9f49226c6c3e4cfa7d261329784e
Serrano Arostegui Max Jairo <mjsamax@gmail.com>
2025-09-17 01:07:44 -0500

    correccion para aceptar codigo 404 y validar el DNS antes de verificar la URL

--- src/http_check.sh
+++ src/http_check.sh
@@ -12,21 +12,18 @@ if [ $# -eq 0 ]; then
     echo "Uso: $0 <dominio.com>" >&2
     exit 1
 fi
+readonly URL="$1"
+# Extraer solo el dominio, sin http:// ni ruta
+readonly DOMAIN=$(echo "$URL" | sed -E 's|https?://([^/]+)/?.*|\1|')
 
-readonly DOMAIN="$1"
-
-echo "Verificando HTTP para ${DOMAIN}"
-
+echo "Verificando HTTP para $URL"
 echo "[Paso 1/2] Verificando DNS..."
 if IP_ADDRESS=$(src/check_dns.sh "$DOMAIN"); then
     echo "DNS OK. La IP de '${DOMAIN}' es: ${IP_ADDRESS}"
 
     echo "[Paso 2/2] Verificando código de estado HTTP..."
+    http_code=$(curl -sL -o /dev/null -w "%{http_code}" "$URL")
 
-    #Obtener código HTTP
-    http_code=$(curl -sL -o /dev/null -w "%{http_code}" "http://${DOMAIN}")
-
-    # Validar resultado
     if [[ "$http_code" == "200" || "$http_code" == "301" ]]; then
         echo "Chequeo HTTP exitoso. Código obtenido: ${http_code} (OK)."
         exit 0
@@ -35,7 +32,6 @@ if IP_ADDRESS=$(src/check_dns.sh "$DOMAIN"); then
         exit 1
     fi
 else
-    #para cuando el DNS da un código de error 1
     echo "Fallo de DNS. No se puede continuar con el chequeo HTTP." >&2
     exit 1
 fi
